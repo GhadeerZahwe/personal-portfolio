63464bfdbaa30faaae43751100d5781b63314aa3
GhadeerZahwe <GhadeerZahwe>
2025-11-18 11:19:42 +0200

    css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,10 @@
 import {useState, useEffect} from 'react';
 import {NavBar,Container} from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
@@ -36,9 +40,9 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-                <a href="#"><img src={''} alt=""/></a>
-                <a href="#"><img src={''} alt=""/></a>
-                <a href="#"><img src={''} alt=""/></a>
+                <a href="#"><img src={navIcon1} alt=""/></a>
+                <a href="#"><img src={navIcon2} alt=""/></a>
+                <a href="#"><img src={navIcon3} alt=""/></a>
             </div>
             <button className="vvd" onClick={() => console.log('Connect')}>
               <span>Let’s Connect</span>
